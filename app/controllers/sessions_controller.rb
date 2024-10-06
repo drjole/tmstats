14770e9142723add1aecc0ede06fbece632d2b63
Jonas Weber <jonas@jole.dev>
2024-10-06 13:16:44 +0200

    Add users and authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  allow_unauthenticated_access only: [:new, :create]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to login_url, alert: "Try again later." }
+
+  def new
+  end
+
+  def create
+    if (user = User.authenticate_by(params.permit(:email_address, :password)))
+      start_new_session_for user
+      redirect_to after_authentication_url, notice: "Successfully logged in."
+    else
+      redirect_to login_url, alert: "Try another email address or password."
+    end
+  end
+
+  def destroy
+    terminate_session
+    redirect_to login_url, notice: "Successfully logged out."
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  allow_unauthenticated_access only: [:new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = if params[:id]
+      User.find(params[:id])
+    else
+      # /profile route
+      Current.user
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to @user, notice: "Successfully registered."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "User edited."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
+  end
+end
