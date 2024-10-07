8e2fc14090c633010682dcebeeae7702ab7e7ff7
Jonas Weber <jonas@jole.dev>
2024-10-07 19:14:03 +0200

    Delete players when deleting games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   has_many :extensions
-  has_many :players
+  has_many :players, dependent: :delete_all
   belongs_to :board
 
   accepts_nested_attributes_for :players, reject_if: :all_blank
