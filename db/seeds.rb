98092cbcf44d2e9737964c276db4569a685ade97
Jonas Weber <jonas@jole.dev>
2024-10-08 11:52:43 +0200

    Make seeds idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,65 @@
 # Base game
-base_game = Extension.create! name: "Base Game"
-Board.create! name: "Tharsis"
-Corporation.create! name: "Credicor", extension: base_game
-Corporation.create! name: "Ecoline", extension: base_game
-Corporation.create! name: "Helion", extension: base_game
-Corporation.create! name: "Mining Guild", extension: base_game
-Corporation.create! name: "Interplanetary Cinematics", extension: base_game
-Corporation.create! name: "Inventrix", extension: base_game
-Corporation.create! name: "Phobolog", extension: base_game
-Corporation.create! name: "Tharsis Republic", extension: base_game
-Corporation.create! name: "Thorgate", extension: base_game
-Corporation.create! name: "United Nations Mars Initiative", extension: base_game
+base_game = Extension.find_or_create_by! name: "Base Game"
+Board.find_or_create_by! name: "Tharsis"
+Corporation.find_or_create_by! name: "Credicor", extension: base_game
+Corporation.find_or_create_by! name: "Ecoline", extension: base_game
+Corporation.find_or_create_by! name: "Helion", extension: base_game
+Corporation.find_or_create_by! name: "Mining Guild", extension: base_game
+Corporation.find_or_create_by! name: "Interplanetary Cinematics", extension: base_game
+Corporation.find_or_create_by! name: "Inventrix", extension: base_game
+Corporation.find_or_create_by! name: "Phobolog", extension: base_game
+Corporation.find_or_create_by! name: "Tharsis Republic", extension: base_game
+Corporation.find_or_create_by! name: "Thorgate", extension: base_game
+Corporation.find_or_create_by! name: "United Nations Mars Initiative", extension: base_game
 
 # Corporate Era
-corporate_era = Extension.create! name: "Corporate Era"
-Corporation.create! name: "Teractor", extension: corporate_era
-Corporation.create! name: "Saturn Systems", extension: corporate_era
+corporate_era = Extension.find_or_create_by! name: "Corporate Era"
+Corporation.find_or_create_by! name: "Teractor", extension: corporate_era
+Corporation.find_or_create_by! name: "Saturn Systems", extension: corporate_era
 
 # Hellas & Elysium
-Extension.create! name: "Hellas & Elysium"
-Board.create! name: "Hellas"
-Board.create! name: "Elysium"
+Extension.find_or_create_by! name: "Hellas & Elysium"
+Board.find_or_create_by! name: "Hellas"
+Board.find_or_create_by! name: "Elysium"
 
 # Venus Next
-venus_next = Extension.create! name: "Venus Next"
-Corporation.create! name: "Aphrodite", extension: venus_next
-Corporation.create! name: "Celestic", extension: base_game
-Corporation.create! name: "Manutech", extension: base_game
-Corporation.create! name: "Morning Star Inc.", extension: base_game
-Corporation.create! name: "Viron", extension: base_game
+venus_next = Extension.find_or_create_by! name: "Venus Next"
+Corporation.find_or_create_by! name: "Aphrodite", extension: venus_next
+Corporation.find_or_create_by! name: "Celestic", extension: base_game
+Corporation.find_or_create_by! name: "Manutech", extension: base_game
+Corporation.find_or_create_by! name: "Morning Star Inc.", extension: base_game
+Corporation.find_or_create_by! name: "Viron", extension: base_game
 
 # Prelude
-prelude = Extension.create! name: "Prelude"
-Corporation.create! name: "Cheung Shing Mars", extension: prelude
-Corporation.create! name: "Point Luna", extension: prelude
-Corporation.create! name: "Robinson Industries", extension: prelude
-Corporation.create! name: "Valley Trust", extension: prelude
-Corporation.create! name: "Vitor", extension: prelude
+prelude = Extension.find_or_create_by! name: "Prelude"
+Corporation.find_or_create_by! name: "Cheung Shing Mars", extension: prelude
+Corporation.find_or_create_by! name: "Point Luna", extension: prelude
+Corporation.find_or_create_by! name: "Robinson Industries", extension: prelude
+Corporation.find_or_create_by! name: "Valley Trust", extension: prelude
+Corporation.find_or_create_by! name: "Vitor", extension: prelude
 
 # Colonies
-colonies = Extension.create! name: "Colonies"
-Corporation.create! name: "Aridor", extension: colonies
-Corporation.create! name: "Arklight", extension: colonies
-Corporation.create! name: "Polyphemos", extension: colonies
-Corporation.create! name: "Poseidon", extension: colonies
-Corporation.create! name: "Stormcraft", extension: colonies
+colonies = Extension.find_or_create_by! name: "Colonies"
+Corporation.find_or_create_by! name: "Aridor", extension: colonies
+Corporation.find_or_create_by! name: "Arklight", extension: colonies
+Corporation.find_or_create_by! name: "Polyphemos", extension: colonies
+Corporation.find_or_create_by! name: "Poseidon", extension: colonies
+Corporation.find_or_create_by! name: "Stormcraft", extension: colonies
 
 # Turmoil
-turmoil = Extension.create! name: "Turmoil"
-Corporation.create! name: "Lakefront Resorts", extension: turmoil
-Corporation.create! name: "Pristar", extension: turmoil
-Corporation.create! name: "Septem Tribus", extension: turmoil
-Corporation.create! name: "Terralabs Research", extension: turmoil
-Corporation.create! name: "Utopia Inveset", extension: turmoil
+turmoil = Extension.find_or_create_by! name: "Turmoil"
+Corporation.find_or_create_by! name: "Lakefront Resorts", extension: turmoil
+Corporation.find_or_create_by! name: "Pristar", extension: turmoil
+Corporation.find_or_create_by! name: "Septem Tribus", extension: turmoil
+Corporation.find_or_create_by! name: "Terralabs Research", extension: turmoil
+Corporation.find_or_create_by! name: "Utopia Inveset", extension: turmoil
 
 # Turmoil Kickstarter Promo Pack
-Corporation.create! name: "Factorum"
-Corporation.create! name: "Mons Insurance"
-Corporation.create! name: "Philares"
+Corporation.find_or_create_by! name: "Factorum"
+Corporation.find_or_create_by! name: "Mons Insurance"
+Corporation.find_or_create_by! name: "Philares"
 
 # TM Promo Pack
-Corporation.create! name: "Arcadian Communities"
-Corporation.create! name: "Splice"
-Corporation.create! name: "Recyclon"
+Corporation.find_or_create_by! name: "Arcadian Communities"
+Corporation.find_or_create_by! name: "Splice"
+Corporation.find_or_create_by! name: "Recyclon"
