0f22ebd1c52631cf5f9ab9cb0d0210c292786f13
Jonas Weber <jonas@jole.dev>
2024-10-09 16:42:28 +0200

    Show a user's games on their profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
+  has_many :players
+  has_many :games, through: :players
+
   validates :email_address, presence: true, uniqueness: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
