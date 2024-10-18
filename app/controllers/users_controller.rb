8d4a4a8a113763342a419c9c7ba9b9fe162f8cba
Jonas Weber <jonas@jole.dev>
2024-10-18 12:02:36 +0200

    Add profile pictures to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email_address, :name, :password, :password_confirmation)
+    params.require(:user).permit(:email_address, :name, :password, :password_confirmation, :profile_picture)
   end
 
   def set_user
