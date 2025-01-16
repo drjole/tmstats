38546eac31d2ff02192bb5a23a04a3403d3b8e98
Jonas Weber <jonas@jole.dev>
2025-01-16 15:27:34 +0100

    Hide data shadow in data zoom in charts

--- app/javascript/controllers/elo_chart_controller.js
+++ app/javascript/controllers/elo_chart_controller.js
@@ -2,73 +2,76 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="elo-chart"
 export default class extends Controller {
-    static values = {
-        data: Object,
+  static values = {
+    data: Object,
+  };
+
+  connect() {
+    let chartOption = {
+      backgroundColor: "transparent",
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          label: {
+            formatter: (params) => {
+              return echarts.time.format(new Date(params.value), "{yyyy}-{MM}-{dd}", true);
+            },
+          },
+        },
+      },
+      legend: {
+        selector: true,
+      },
+      grid: {
+        containLabel: true,
+        right: 20,
+        left: 20,
+      },
+      dataZoom: [
+        {
+          filterMode: "none",
+          showDataShadow: false,
+          labelFormatter: (value, _) => {
+            return echarts.time.format(new Date(value), "{yyyy}-{MM}-{dd}", true, "DE");
+          },
+        },
+      ],
+      xAxis: {
+        type: "time",
+      },
+      yAxis: {
+        interval: 30,
+        min: (value) => Math.trunc((value.min - 1000) / 30) * 30 + 1000 - 30,
+        max: (value) => Math.trunc((value.max - 1000) / 30) * 30 + 1000 + 30,
+      },
+      series: Object.keys(this.dataValue).map((userId) => {
+        return {
+          data: this.dataValue[userId].map((row) => Object.values(row).slice(0, 2)),
+          type: "line",
+          step: "end",
+          name: this.dataValue[userId][0].user_name,
+        };
+      }),
     };
 
-    connect() {
-        let chartOption = {
-            backgroundColor: "transparent",
-            tooltip: {
-                trigger: "axis",
-                axisPointer: {
-                    label: {
-                        formatter: (params) => {
-                            return echarts.time.format(new Date(params.value), "{yyyy}-{MM}-{dd}", true);
-                        },
-                    },
-                },
-            },
-            legend: {
-                selector: true,
-            },
-            grid: {
-                containLabel: true,
-                right: 20,
-                left: 20,
-            },
-            dataZoom: {
-                filterMode: "none",
-                labelFormatter: (value, _) => {
-                    return echarts.time.format(new Date(value), "{yyyy}-{MM}-{dd}", true, "DE");
-                },
-            },
-            xAxis: {
-                type: "time",
-            },
-            yAxis: {
-                interval: 30,
-                min: (value) => Math.trunc((value.min - 1000) / 30) * 30 + 1000 - 30,
-                max: (value) => Math.trunc((value.max - 1000) / 30) * 30 + 1000 + 30,
-            },
-            series: Object.keys(this.dataValue).map((userId) => {
-                return {
-                    data: this.dataValue[userId].map((row) => Object.values(row).slice(0, 2)),
-                    type: "line",
-                    step: "end",
-                    name: this.dataValue[userId][0].user_name,
-                };
-            }),
-        };
+    this.chart = echarts.init(this.element, "dark");
+    this.chart.setOption(chartOption);
 
-        this.chart = echarts.init(this.element, "dark");
-        this.chart.setOption(chartOption);
+    this.chart.on("click", (params) => this.openGame(params.value.game_id));
+  }
 
-        this.chart.on("click", (params) => this.openGame(params.value.game_id));
-    }
+  disconnect() {
+    this.chart.dispose();
+  }
 
-    disconnect() {
-        this.chart.dispose();
-    }
+  resize() {
+    this.chart.resize();
+  }
 
-    resize() {
-        this.chart.resize();
-    }
-
-    openGame(gameId) {
-        const id = `game_${gameId}`;
-        location.hash = "";
-        location.hash = `#${id}`;
-        document.getElementById(id).querySelector("details").open = true;
-    }
+  openGame(gameId) {
+    const id = `game_${gameId}`;
+    location.hash = "";
+    location.hash = `#${id}`;
+    document.getElementById(id).querySelector("details").open = true;
+  }
 }
--- app/javascript/controllers/user_elo_chart_controller.js
+++ app/javascript/controllers/user_elo_chart_controller.js
@@ -2,75 +2,76 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="user-elo-chart"
 export default class extends Controller {
-    static values = {
-        data: Array,
+  static values = {
+    data: Array,
+  };
+
+  connect() {
+    let chartOption = {
+      backgroundColor: "transparent",
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          label: {
+            formatter: (params) => {
+              return echarts.time.format(new Date(params.value), "{yyyy}-{MM}-{dd}", true);
+            },
+          },
+        },
+      },
+      grid: {
+        containLabel: true,
+        top: "10px",
+        right: 20,
+        left: 20,
+      },
+      dataZoom: {
+        filterMode: "none",
+        showDataShadow: false,
+        labelFormatter: (value, _) => {
+          return echarts.time.format(new Date(value), "{yyyy}-{MM}-{dd}", true, "DE");
+        },
+      },
+      xAxis: {
+        type: "time",
+      },
+      yAxis: {
+        interval: 30,
+        min: (value) => Math.trunc((value.min - 1000) / 30) * 30 + 1000 - 30,
+        max: (value) => Math.trunc((value.max - 1000) / 30) * 30 + 1000 + 30,
+      },
+      dataset: {
+        source: this.dataValue,
+      },
+      series: {
+        type: "line",
+        step: "end",
+        name: "Rating",
+        encode: {
+          x: "time",
+          y: "Rating",
+        },
+      },
     };
 
-    connect() {
-        let chartOption = {
-            backgroundColor: "transparent",
-            tooltip: {
-                trigger: "axis",
-                axisPointer: {
-                    label: {
-                        formatter: (params) => {
-                            return echarts.time.format(new Date(params.value), "{yyyy}-{MM}-{dd}", true);
-                        },
-                    },
-                },
-            },
-            grid: {
-                containLabel: true,
-                top: "10px",
-                right: 20,
-                left: 20,
-            },
-            dataZoom: {
-                filterMode: "none",
-                labelFormatter: (value, _) => {
-                    return echarts.time.format(new Date(value), "{yyyy}-{MM}-{dd}", true, "DE");
-                },
-            },
-            xAxis: {
-                type: "time",
-            },
-            yAxis: {
-                interval: 30,
-                min: (value) => Math.trunc((value.min - 1000) / 30) * 30 + 1000 - 30,
-                max: (value) => Math.trunc((value.max - 1000) / 30) * 30 + 1000 + 30,
-            },
-            dataset: {
-                source: this.dataValue,
-            },
-            series: {
-                type: "line",
-                step: "end",
-                name: "Rating",
-                encode: {
-                    x: "time",
-                    y: "Rating",
-                },
-            },
-        };
+    this.chart = echarts.init(this.element, "dark");
+    this.chart.setOption(chartOption);
 
-        this.chart = echarts.init(this.element, "dark");
-        this.chart.setOption(chartOption);
+    this.chart.on("click", (params) => this.openGame(params.value.game_id));
+  }
 
-        this.chart.on("click", (params) => this.openGame(params.value.game_id));
-    }
+  disconnect() {
+    this.chart.dispose();
+  }
 
-    disconnect() {
-        this.chart.dispose();
-    }
+  resize() {
+    this.chart.resize();
+  }
 
-    resize() {
-        this.chart.resize();
-    }
-
-    openGame(gameId) {
-        const id = `game_${gameId}`;
-        location.hash = "";
-        location.hash = `#${id}`;
-        document.getElementById(id).querySelector("details").open = true;
-    }
+  openGame(gameId) {
+    const id = `game_${gameId}`;
+    location.hash = "";
+    location.hash = `#${id}`;
+    document.getElementById(id).querySelector("details").open = true;
+  }
 }
